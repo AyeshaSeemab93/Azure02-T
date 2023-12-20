65a637ed997159b2801c883a3c1cc273494ae046
AyeshaSeemab93 <ayeshaseemab2017@gmail.com>
2023-12-20 14:35:21 +0200

    1

--- obj/Debug/net8.0/Azure02.AssemblyInfo.cs
+++ obj/Debug/net8.0/Azure02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Azure02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+425ace11e880ae5719175d7a4631c61670f39365")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Azure02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Azure02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
